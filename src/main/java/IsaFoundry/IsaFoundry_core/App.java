8c39ae1436f8120850d337e61757cf5eb04ebb0d
Pablo Guerra Prieto <scrhall@scrhall.com>
2014-01-19 14:05:50 +0100

    Test de deployments y realizcion de tareas programadas

--- src/main/java/IsaFoundry/IsaFoundry_core/App.java
+++ src/main/java/IsaFoundry/IsaFoundry_core/App.java
@@ -1,12 +1,11 @@
 package IsaFoundry.IsaFoundry_core;
 
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
-import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.RepositoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,9 +24,26 @@ public class App
         repositoryService.createDeployment()
           .addClasspathResource("FinalizacionProyecto.bpmn")
           .deploy();
-              
         Log.info("Number of process definitions: " + repositoryService.createProcessDefinitionQuery().count());            
-                
+        repositoryService.createDeployment()
+        .addClasspathResource("CreacionProyecto.bpmn")
+        .deploy();
+      Log.info("Number of process definitions: " + repositoryService.createProcessDefinitionQuery().count());     
+      repositoryService.createDeployment()
+      .addClasspathResource("Reuniones.bpmn")
+      .deploy();
+    Log.info("Number of process definitions: " + repositoryService.createProcessDefinitionQuery().count());            
+    
+        ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
+        exec.scheduleAtFixedRate(new Runnable() {
+          @Override
+          public void run() {
+            //TODO:Comprobar tareas pendientes en el motor de activiti
+        	//TODO:Comprobar emails y formularios para realizar tareas pendientes
+        	
+          }
+        }, 0, 5, TimeUnit.SECONDS);      
+             
                   
     }
 }
